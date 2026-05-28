88938c2d80b05296546b145a046c007f4a32db39
LorettoMolina <23308060610601@cetis61.edu.mx>
2026-05-28 10:19:42 -0500

    111

--- database/biblioteca (1).sql
+++ database/biblioteca (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-05-2026 a las 19:21:52
+-- Tiempo de generación: 28-05-2026 a las 17:19:07
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -53,7 +53,8 @@ CREATE TABLE `libros` (
   `id` int(11) NOT NULL,
   `titulo` varchar(255) DEFAULT NULL,
   `autor` varchar(255) DEFAULT NULL,
-  `categoria` varchar(100) DEFAULT NULL
+  `categoria` varchar(100) DEFAULT NULL,
+  `stock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -78,7 +79,10 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `contrasena`, `apellido`, `telefono`, `rol`) VALUES
 (1, 'loretto', 'loretto@gmail.com', 'loretto', NULL, NULL, 'usuario'),
-(2, 'gfgfdhgf', '23308060610601@cetis61.edu.mx', 'loretto,27-', 'hgfjhfj', '6565895625', 'usuario');
+(2, 'gfgfdhgf', '23308060610601@cetis61.edu.mx', 'loretto,27-', 'hgfjhfj', '6565895625', 'usuario'),
+(3, 'luis', 'lopes@gmail.com', '$2b$12$N9EqOYnMA6gqEzqlijOurew5hDZZ7HB3M9AoB0m6POUiCnf8mL9EO', 'lopes', '6568596523', 'usuario'),
+(4, 'loretto', '', '$2b$12$Vi8CH3JPpyzKzneSD0FnF.SSZlNx1cdfzAtyqCj/wOqM6eBDRCfRS', '', '', 'usuario'),
+(5, 'luis', 'luis@gmail.com', '$2b$12$96Qby0eCP/P7ubUNdbx3Rex4VpjRzbLGHy58JinZqDI5TTvkgNIoO', 'avila', '6561234567', 'usuario');
 
 --
 -- Índices para tablas volcadas
@@ -117,13 +121,13 @@ ALTER TABLE `incidencias`
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
